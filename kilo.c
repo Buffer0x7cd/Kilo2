c1f1f60a8b700521b98ba7e52adc393375911159
buffer0x7cd <manvendra021997@gmail.com>
2017-12-12 02:06:39 +0530

    enabled exit by pressing Ctrl+q

--- kilo.c
+++ kilo.c
@@ -3,17 +3,37 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <errno.h>
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 
 
 struct termios orig_termios;
+
+/*
+ * Helper method for printing the error and
+ * exit the program */
+void die(const char *msg)
+{
+	perror(msg);
+	exit(1);
+}
+
+
+
+/*cleanup routine to store the default behavaior
+ * of user's terminal */
 void disableRawMode()
 {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1 ) die("tcsetattr");
 }
+
+
 void enableRawMode()
 {
-	struct termios raw; 
-	tcgetattr(STDIN_FILENO, &orig_termios);
+	struct termios raw;
+        if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");	
 	raw = orig_termios;
 
 	atexit(disableRawMode);
@@ -21,9 +41,11 @@ void enableRawMode()
 	raw.c_oflag &= ~(OPOST);
 	raw.c_cflag |= (CS8);
 	raw.c_lflag &= ~(ECHO|ICANON|IEXTEN|ISIG);
+
 	raw.c_cc[VMIN] = 0;
 	raw.c_cc[VTIME] = 1;
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 int main(void)
@@ -32,7 +54,8 @@ int main(void)
 	while(1)
 	{
 		char c = '\0';
-		read(STDIN_FILENO, &c, 1);
+		if(read(STDIN_FILENO, &c, 1) == -1)
+				die("read error");
 		if(iscntrl(c))
 		{
 			printf("%d\r\n",c);
@@ -41,7 +64,7 @@ int main(void)
 		{
 			printf("%d ('%c')\r\n",c,c);
 		}
-		if (c == 'q') break;
+		if (c == CTRL_KEY('q')) break;
 	}
 	return 0;
 }
